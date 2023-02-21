fcdb4aff015e69a6334f295535cda32d748eaa64
Dmitriy Kozhanov <medic8463@gmail.com>
2023-02-21 11:09:59 +0300

    [fix] Config file fixes

--- config/database.php
+++ config/database.php
@@ -66,7 +66,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'db'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
--- config/filament.php
+++ config/filament.php
@@ -193,8 +193,8 @@ return [
         ],
         'max_content_width' => 'full',
         'notifications' => [
-            'vertical_alignment' => 'top',
-            'alignment' => 'right',
+            'vertical_alignment' => 'bottom',
+            'alignment' => 'center',
         ],
         'sidebar' => [
             'is_collapsible_on_desktop' => false,
